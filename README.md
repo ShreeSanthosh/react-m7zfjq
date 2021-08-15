982b703c9834703c27955b7c6a7e2cf368725838
ShreeSanthosh <shreesandy355@gmail.com>
2021-08-15 10:50:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-m7zfjq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-m7zfjq)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+const api ={
+  key:"8e6df62e0577a740e6a98dc048bd3cc3",
+  base:"http://api.openweatherapp.org/data/2.5/"
+}
+
+export default function App() {
+  return (
+    <div className="app">
+      
+    </div>
+  );
+}
